a043e61d0d719cb82c90d0ea8641f2ae5f555fa7
sissi <antsositraka@gmail.com>
2023-06-06 06:44:50 +0200

    rect

--- test222dev.php
+++ test222dev.php
@@ -1,8 +1,4 @@
 <?php echo 'if you want to serve PHP code in XHTML or XML documents,
-<<<<<<< HEAD
                 use these tags'; 
                 echo 'test push ok'
-                ?>
-=======
-                use these tags'; echo "eooooooooooooo"; ?>
->>>>>>> test
+                 ?>
